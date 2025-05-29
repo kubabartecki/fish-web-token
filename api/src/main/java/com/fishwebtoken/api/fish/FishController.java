bef2f80bce54f472b8d0e62bdc3ff16967c702ea
kubabartecki <169511@stud.prz.edu.pl>
2025-05-30 00:58:46 +0200

    mock for testing api response caching

--- api/src/main/java/com/fishwebtoken/api/fish/FishController.java
+++ api/src/main/java/com/fishwebtoken/api/fish/FishController.java
@@ -1,10 +1,13 @@
 package com.fishwebtoken.api.fish;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/v1/fish")
@@ -14,5 +17,12 @@ public class FishController {
     @PreAuthorize("hasAuthority('user:read')")
     public String getFishById(@PathVariable String id) {
         return "Fish details for ID: " + id;
+    }
+
+    @GetMapping("/{id}/cached")
+    @PreAuthorize("hasAuthority('user:read')")
+    @Cacheable("fishes")
+    public String getFishByIdCached(@PathVariable String id) {
+        return "Cached Fish, time: " + LocalDateTime.now() + " details for ID: " + id;
     }
 }
