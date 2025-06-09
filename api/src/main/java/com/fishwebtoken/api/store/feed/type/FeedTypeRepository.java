9cd1e5bcb8c54eb048e2503a2d6676b58d6af55d
kubabartecki <169511@stud.prz.edu.pl>
2025-06-09 22:53:22 +0200

    Implement FeedType CRUD

---
+++ api/src/main/java/com/fishwebtoken/api/store/feed/type/FeedTypeRepository.java
@@ -0,0 +1,6 @@
+package com.fishwebtoken.api.store.feed.type;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedTypeRepository extends JpaRepository<FeedType, Integer> {
+}
--- api/src/main/java/com/fishwebtoken/api/store/feed/type/FeedTypeService.java
+++ api/src/main/java/com/fishwebtoken/api/store/feed/type/FeedTypeService.java
@@ -1,13 +1,37 @@
 package com.fishwebtoken.api.store.feed.type;
 
+import com.fishwebtoken.api.config.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class FeedTypeService {
+    private final FeedTypeRepository feedTypeRepository;
+
     public List<FeedType> getFeedTypes() {
-        // todo
-        return List.of();
+        return feedTypeRepository.findAll();
+    }
+
+    public FeedType addFeedType(String name) {
+        FeedType feedType = new FeedType();
+        feedType.setName(name);
+        return feedTypeRepository.save(feedType);
+    }
+
+    public FeedType updateFeedType(Integer id, String name) {
+        FeedType feedType = feedTypeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Feed type not found with ID: " + id));
+
+        feedType.setName(name);
+        return feedTypeRepository.save(feedType);
+    }
+
+    public void deleteFeedType(Integer id) {
+        FeedType feedType = feedTypeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Feed type not found with ID: " + id));
+        feedTypeRepository.delete(feedType);
     }
 }
